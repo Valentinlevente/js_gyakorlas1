8a4b0bdc0c0031e96aa1c051d41e8a214906b8f1
Valentinusz <valentin.levente2003@gmail.com>
2022-09-08 12:31:00 +0200

    3. feladat

---
+++ feladat3.js
@@ -0,0 +1,19 @@
+function hurra(){
+    console.log("Hurrá!")
+}
+
+//let time = setInterval(hurra,2000);
+
+
+//3a
+let counter = 0;
+
+function szamol(){
+    console.log(counter);
+    counter++;
+}
+
+let time2 = setInterval(szamol,2000);
+
+
+
